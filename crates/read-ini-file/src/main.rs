2080282aedf5172ae10ecc10be45be0cba3b0991
Ngakan Gandhi <ngandhi@pmintl.net>
2024-10-17 16:00:54 +0700

    add examples for ini file reader

---
+++ crates/read-ini-file/src/main.rs
@@ -0,0 +1,20 @@
+use configparser::ini::Ini;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut config = Ini::new();
+
+    // Load file from configuration path
+    let configmap = config.load("config.ini")?;
+    for k in configmap.keys() {
+        println!("{}", k);
+    };
+
+    // Test: Load AWS configuration file
+    let awsconfigmap = config.load("/home/ngandhi/.aws/config")?;
+    for k in awsconfigmap.keys() {
+        println!("{}", k);
+    };
+
+    Ok(())
+}
